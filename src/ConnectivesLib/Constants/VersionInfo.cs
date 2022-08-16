2a9149cdc55560cae3e3ae9585f39c5309e14b8a
usama <mahmoodify@gmail.com>
2022-08-16 19:11:07 +0500

    adding version info

--- src/ConnectivesLib/Constants/VersionInfo.cs
+++ src/ConnectivesLib/Constants/VersionInfo.cs
@@ -2,5 +2,10 @@
 {
     public class VersionInfo
     {
+        public const int VersionMajorNumber = 1;
+        public const int VersionMinorNumber = 0;
+        public const int VersionPatchNumber = 0;
+
+        public string VersionNumber { get { return $"{VersionMajorNumber}.{VersionMinorNumber}.{VersionPatchNumber}"; } }
     }
 }
