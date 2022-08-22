5bf4d045f86b611fbbc48477bab63187470f3c52
usama <mahmoodify@gmail.com>
2022-08-22 09:34:33 +0500

    cleanup

--- src/ConnectivesLib/Settings/ConnectionSettings.cs
+++ src/ConnectivesLib/Settings/ConnectionSettings.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Connectives.Settings
+﻿namespace Connectives.Settings
 {
     public class ConnectionSettings
     {
