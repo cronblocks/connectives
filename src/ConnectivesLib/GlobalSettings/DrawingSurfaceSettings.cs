def630e0a35898c413e69d5d869018901b9527a8
usama <mahmoodify@gmail.com>
2022-08-31 01:44:15 +0500

    adding default color for min div

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -7,6 +7,6 @@ namespace Connectives.GlobalSettings
         public Color SurfaceColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
 
         public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 150, red: 250, green: 162, blue: 65);
-        public Color MinorDivisionsColor { get; set; }
+        public Color MinorDivisionsColor { get; set; } = Color.FromArgb(alpha: 150, red: 253, green: 219, blue: 181);
     }
 }
