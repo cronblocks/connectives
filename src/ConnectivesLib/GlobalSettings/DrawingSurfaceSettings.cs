5e24b15439e89c21b99b29ac0aa81675fdd9cd93
usama <mahmoodify@gmail.com>
2022-08-31 01:23:05 +0500

    adding colors

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -1,6 +1,12 @@
-﻿namespace Connectives.GlobalSettings
+﻿using System.Drawing;
+
+namespace Connectives.GlobalSettings
 {
     public class DrawingSurfaceSettings
     {
+        public Color SurfaceColor { get; set; }
+
+        public Color MajorDivisionsColor { get; set; }
+        public Color MinorDivisionsColor { get; set; }
     }
 }
