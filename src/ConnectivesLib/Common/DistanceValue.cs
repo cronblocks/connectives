75eb2e77762d48ca611de23d5a5746502a37bc1c
usama <mahmoodify@gmail.com>
2022-08-31 01:50:21 +0500

    adding distance value

---
+++ src/ConnectivesLib/Common/DistanceValue.cs
@@ -0,0 +1,8 @@
+﻿namespace Connectives.Common
+{
+    public class DistanceValue
+    {
+        public DistanceUnit DistanceUnit { get; set; }
+        public double Distance { get; set; }
+    }
+}
