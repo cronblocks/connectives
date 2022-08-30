e161ba577330f916029e57f339405cdc1164fd38
usama <mahmoodify@gmail.com>
2022-08-31 01:44:54 +0500

    adjusting alpha values for colors

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -6,7 +6,7 @@ namespace Connectives.GlobalSettings
     {
         public Color SurfaceColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
 
-        public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 150, red: 250, green: 162, blue: 65);
-        public Color MinorDivisionsColor { get; set; } = Color.FromArgb(alpha: 150, red: 253, green: 219, blue: 181);
+        public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 180, red: 250, green: 162, blue: 65);
+        public Color MinorDivisionsColor { get; set; } = Color.FromArgb(alpha: 120, red: 253, green: 219, blue: 181);
     }
 }
