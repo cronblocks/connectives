2f9d25225a9c5414ec4c116bffb895ac9410f552
usama <mahmoodify@gmail.com>
2022-08-31 02:14:37 +0500

    separating divisions background and line colors

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -13,8 +13,11 @@ namespace Connectives.GlobalSettings
         // ---------------
         // Divisions
         // ------------
-        public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 180, red: 250, green: 162, blue: 65);
-        public Color MinorDivisionsColor { get; set; } = Color.FromArgb(alpha: 120, red: 253, green: 219, blue: 181);
+        public Color MajorDivisionsBackgroundColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
+        public Color MinorDivisionsBackgroundColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
+
+        public Color MajorDivisionsBorderLineColor { get; set; } = Color.FromArgb(alpha: 180, red: 250, green: 162, blue: 65);
+        public Color MinorDivisionsBorderLineColor { get; set; } = Color.FromArgb(alpha: 120, red: 253, green: 219, blue: 181);
 
         public Length DistanceBetweenMajorDivisions { get; set; } = new Length{ Unit = LengthUnit.Inch, Measurement = 1 };
         public int MinorDivisionsBetweenMajorDivisions { get; set; } = 12;
