5f7a6141d42ade5d97da66ac823bcbc4b4253425
usama <mahmoodify@gmail.com>
2022-08-31 01:42:59 +0500

    adding default color for maj div

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -6,7 +6,7 @@ namespace Connectives.GlobalSettings
     {
         public Color SurfaceColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
 
-        public Color MajorDivisionsColor { get; set; }
+        public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 150, red: 250, green: 162, blue: 65);
         public Color MinorDivisionsColor { get; set; }
     }
 }
