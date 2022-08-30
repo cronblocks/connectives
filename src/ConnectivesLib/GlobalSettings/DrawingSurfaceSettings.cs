7077cc2a0ac1b9d8ac50bc67b27e9638a33813ac
usama <mahmoodify@gmail.com>
2022-08-31 01:48:46 +0500

    defining portions

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -5,11 +5,13 @@ namespace Connectives.GlobalSettings
     public class DrawingSurfaceSettings
     {
         // ---------------
-        // Colors
+        // Background Surface
         // ------------
-
         public Color SurfaceColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
 
+        // ---------------
+        // Divisions
+        // ------------
         public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 180, red: 250, green: 162, blue: 65);
         public Color MinorDivisionsColor { get; set; } = Color.FromArgb(alpha: 120, red: 253, green: 219, blue: 181);
     }
