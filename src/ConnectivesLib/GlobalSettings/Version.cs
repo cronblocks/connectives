4d197c86055677f908eff9723cc99b0c5be8c632
usama <mahmoodify@gmail.com>
2022-08-31 01:20:06 +0500

    adding global settings

---
+++ src/ConnectivesLib/GlobalSettings/Version.cs
@@ -0,0 +1,11 @@
+﻿namespace Connectives.GlobalSettings
+{
+    public class Version
+    {
+        public const int MajorNumber = 1;
+        public const int MinorNumber = 0;
+        public const int PatchNumber = 0;
+
+        public string VersionString { get { return $"{MajorNumber}.{MinorNumber}.{PatchNumber}"; } }
+    }
+}
