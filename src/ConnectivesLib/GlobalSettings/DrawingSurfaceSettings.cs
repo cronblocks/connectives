6f2be490356bf9e66a3f21ca05f37a6b698a57e8
usama <mahmoodify@gmail.com>
2022-08-31 01:41:43 +0500

    setting default surface colot

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -4,7 +4,7 @@ namespace Connectives.GlobalSettings
 {
     public class DrawingSurfaceSettings
     {
-        public Color SurfaceColor { get; set; }
+        public Color SurfaceColor { get; set; } = Color.FromArgb(alpha: 150, red: 228, green: 221, blue: 135);
 
         public Color MajorDivisionsColor { get; set; }
         public Color MinorDivisionsColor { get; set; }
