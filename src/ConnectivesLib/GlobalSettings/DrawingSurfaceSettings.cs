9c218aaec918deff6e6b67862242d214ba916db5
usama <mahmoodify@gmail.com>
2022-08-31 02:05:00 +0500

    adding divisions

--- src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
+++ src/ConnectivesLib/GlobalSettings/DrawingSurfaceSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using Connectives.Common;
+using System.Drawing;
 
 namespace Connectives.GlobalSettings
 {
@@ -14,5 +15,8 @@ namespace Connectives.GlobalSettings
         // ------------
         public Color MajorDivisionsColor { get; set; } = Color.FromArgb(alpha: 180, red: 250, green: 162, blue: 65);
         public Color MinorDivisionsColor { get; set; } = Color.FromArgb(alpha: 120, red: 253, green: 219, blue: 181);
+
+        public Length DistanceBetweenMajorDivisions { get; set; } = new Length{ Unit = LengthUnit.Inch, Measurement = 1 };
+        public int MinorDivisionsBetweenMajorDivisions { get; set; } = 10;
     }
 }
