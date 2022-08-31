49ab61e55e2da4214697869d4ce3b38ffec32bc5
usama <mahmoodify@gmail.com>
2022-08-31 19:20:37 +0500

    adding width class

---
+++ src/ConnectivesLib/Common/Width.cs
@@ -0,0 +1,8 @@
+﻿namespace Connectives.Common
+{
+    public class Width
+    {
+        public WidthUnit Unit { get; set; } = WidthUnit.MilliMeter;
+        public double Measurement { get; set; } = 0;
+    }
+}
