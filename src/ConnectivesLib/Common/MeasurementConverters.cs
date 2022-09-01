ca758125f2c60cf0cd7e6ce2df38f45efbbf1007
usama <mahmoodify@gmail.com>
2022-09-01 18:57:29 +0500

    returning null to be on safe side

--- src/ConnectivesLib/Common/MeasurementConverters.cs
+++ src/ConnectivesLib/Common/MeasurementConverters.cs
@@ -4,22 +4,22 @@
     {
         static WidthMeasurement ConvertTo(this WidthMeasurement measurement, DistanceUnit targetUnit)
         {
-            return measurement;
+            return null;
         }
 
         static LengthMeasurement ConvertTo(this LengthMeasurement measurement, DistanceUnit targetUnit)
         {
-            return measurement;
+            return null;
         }
 
         static AngleMeasurement ConvertTo(this AngleMeasurement measurement, AngleUnit targetUnit)
         {
-            return measurement;
+            return null;
         }
 
         static TimeMeasurement ConvertTo(this TimeMeasurement measurement, TimeUnit targetUnit)
         {
-            return measurement;
+            return null;
         }
     }
 }
