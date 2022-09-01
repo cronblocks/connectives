3fed80eba28054491d5693ce2de62f980699c27e
usama <mahmoodify@gmail.com>
2022-09-02 01:46:47 +0500

    adding conversion factors for symmetric distance units - other placeholders added

--- src/ConnectivesLib/Common/MeasurementUnits.cs
+++ src/ConnectivesLib/Common/MeasurementUnits.cs
@@ -26,6 +26,72 @@
 
     internal static class ConversionFactors
     {
+        public static double GetDistanceConversionFactor(DistanceUnit from, DistanceUnit to)
+        {
+            switch (from)
+            {
+                case DistanceUnit.Pixel:
+                    switch (to)
+                    {
+                        case DistanceUnit.Pixel:                  return 1.0;
+                        case DistanceUnit.PercentageCanvasWidth:  return 0.0;
+                        case DistanceUnit.PercentageCanvasHeight: return 0.0;
+                        case DistanceUnit.Inch:                   return 0.0;
+                        case DistanceUnit.MilliMeter:             return 0.0;
+                        default:                                  return 0.0;
+                    }
+                case DistanceUnit.PercentageCanvasWidth:
+                    switch (to)
+                    {
+                        case DistanceUnit.Pixel:                  return 0.0;
+                        case DistanceUnit.PercentageCanvasWidth:  return 1.0;
+                        case DistanceUnit.PercentageCanvasHeight: return 0.0;
+                        case DistanceUnit.Inch:                   return 0.0;
+                        case DistanceUnit.MilliMeter:             return 0.0;
+                        default:                                  return 0.0;
+                    }
+                case DistanceUnit.PercentageCanvasHeight:
+                    switch (to)
+                    {
+                        case DistanceUnit.Pixel:                  return 0.0;
+                        case DistanceUnit.PercentageCanvasWidth:  return 0.0;
+                        case DistanceUnit.PercentageCanvasHeight: return 1.0;
+                        case DistanceUnit.Inch:                   return 0.0;
+                        case DistanceUnit.MilliMeter:             return 0.0;
+                        default:                                  return 0.0;
+                    }
+                case DistanceUnit.Inch:
+                    switch (to)
+                    {
+                        case DistanceUnit.Pixel:                  return 0.0;
+                        case DistanceUnit.PercentageCanvasWidth:  return 0.0;
+                        case DistanceUnit.PercentageCanvasHeight: return 0.0;
+                        case DistanceUnit.Inch:                   return 1.0;
+                        case DistanceUnit.MilliMeter:             return 0.0;
+                        default:                                  return 0.0;
+                    }
+                case DistanceUnit.MilliMeter:
+                    switch (to)
+                    {
+                        case DistanceUnit.Pixel:                  return 0.0;
+                        case DistanceUnit.PercentageCanvasWidth:  return 0.0;
+                        case DistanceUnit.PercentageCanvasHeight: return 0.0;
+                        case DistanceUnit.Inch:                   return 0.0;
+                        case DistanceUnit.MilliMeter:             return 1.0;
+                        default:                                  return 0.0;
+                    }
+                default: return 0.0;
+            }
+        }
 
+        public static double GetAngleConversionFactor(AngleUnit from, AngleUnit to)
+        {
+
+        }
+
+        public static double GetTimeConversionFactor(TimeUnit from, TimeUnit to)
+        {
+
+        }
     }
 }
