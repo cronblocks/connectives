00cebc18bc5d93dc3995dbcfd00d961a3be7f8be
usama <mahmoodify@gmail.com>
2022-09-02 01:30:21 +0500

    making version a static class

--- src/ConnectivesLib/GlobalSettings/Version.cs
+++ src/ConnectivesLib/GlobalSettings/Version.cs
@@ -1,11 +1,11 @@
 ﻿namespace Connectives.GlobalSettings
 {
-    public class Version
+    public static class Version
     {
         public const int MajorNumber = 1;
         public const int MinorNumber = 0;
         public const int PatchNumber = 0;
 
-        public string VersionString { get { return $"{MajorNumber}.{MinorNumber}.{PatchNumber}"; } }
+        public static string VersionString { get { return $"{MajorNumber}.{MinorNumber}.{PatchNumber}"; } }
     }
 }
