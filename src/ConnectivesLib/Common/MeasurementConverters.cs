9732bda5aaa6fcd74fd79a514b3add3c71b34f47
usama <mahmoodify@gmail.com>
2022-09-01 18:59:19 +0500

    returning original measurement when target unit is same as original unit

--- src/ConnectivesLib/Common/MeasurementConverters.cs
+++ src/ConnectivesLib/Common/MeasurementConverters.cs
@@ -4,21 +4,41 @@
     {
         static WidthMeasurement ConvertTo(this WidthMeasurement measurement, DistanceUnit targetUnit)
         {
+            if (measurement.Unit == targetUnit)
+            {
+                return measurement;
+            }
+
             return null;
         }
 
         static LengthMeasurement ConvertTo(this LengthMeasurement measurement, DistanceUnit targetUnit)
         {
+            if (measurement.Unit == targetUnit)
+            {
+                return measurement;
+            }
+
             return null;
         }
 
         static AngleMeasurement ConvertTo(this AngleMeasurement measurement, AngleUnit targetUnit)
         {
+            if (measurement.Unit == targetUnit)
+            {
+                return measurement;
+            }
+
             return null;
         }
 
         static TimeMeasurement ConvertTo(this TimeMeasurement measurement, TimeUnit targetUnit)
         {
+            if (measurement.Unit == targetUnit)
+            {
+                return measurement;
+            }
+            
             return null;
         }
     }
