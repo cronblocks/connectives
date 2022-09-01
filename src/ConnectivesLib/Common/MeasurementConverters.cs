8999e37132e69a88abe9fd6da2569e1473e8d508
usama <mahmoodify@gmail.com>
2022-09-01 19:03:03 +0500

    changing measurement conversions to apply conversions onto original instances

--- src/ConnectivesLib/Common/MeasurementConverters.cs
+++ src/ConnectivesLib/Common/MeasurementConverters.cs
@@ -2,44 +2,32 @@
 {
     public static class MeasurementConverters
     {
-        static WidthMeasurement ConvertTo(this WidthMeasurement measurement, DistanceUnit targetUnit)
+        static void ConvertTo(this WidthMeasurement measurement, DistanceUnit targetUnit)
         {
-            if (measurement.Unit == targetUnit)
+            if (measurement.Unit != targetUnit)
             {
-                return measurement;
             }
-
-            return null;
         }
 
-        static LengthMeasurement ConvertTo(this LengthMeasurement measurement, DistanceUnit targetUnit)
+        static void ConvertTo(this LengthMeasurement measurement, DistanceUnit targetUnit)
         {
-            if (measurement.Unit == targetUnit)
+            if (measurement.Unit != targetUnit)
             {
-                return measurement;
             }
-
-            return null;
         }
 
-        static AngleMeasurement ConvertTo(this AngleMeasurement measurement, AngleUnit targetUnit)
+        static void ConvertTo(this AngleMeasurement measurement, AngleUnit targetUnit)
         {
-            if (measurement.Unit == targetUnit)
+            if (measurement.Unit != targetUnit)
             {
-                return measurement;
             }
-
-            return null;
         }
 
-        static TimeMeasurement ConvertTo(this TimeMeasurement measurement, TimeUnit targetUnit)
+        static void ConvertTo(this TimeMeasurement measurement, TimeUnit targetUnit)
         {
-            if (measurement.Unit == targetUnit)
+            if (measurement.Unit != targetUnit)
             {
-                return measurement;
             }
-            
-            return null;
         }
     }
 }
