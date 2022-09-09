0e3a5fe59ad3b8acbbf34cbb86e9062733f16fb7
usama <mahmoodify@gmail.com>
2022-09-09 14:09:03 +0500

    adding border line width

--- src/ConnectivesLib/GlobalSettings/BlockSettings.cs
+++ src/ConnectivesLib/GlobalSettings/BlockSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using Connectives.Common;
+using System.Drawing;
 
 namespace Connectives.GlobalSettings
 {
@@ -8,5 +9,10 @@ namespace Connectives.GlobalSettings
         // Colors
         // ------------
         public Color BlockBorderColor { get; set; } = Color.FromArgb(alpha: 200, red: 250, green: 35, blue: 35);
+
+        // ---------------
+        // Dimensions
+        // ------------
+        public WidthMeasurement BlockBorderLineWidth { get; set; } = new WidthMeasurement { Unit = DistanceUnit.MilliMeter, Measurement = 0.08 };
     }
 }
