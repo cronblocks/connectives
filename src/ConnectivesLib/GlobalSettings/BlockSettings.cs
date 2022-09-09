9dc1e81333996b037bf3e15b0d4afd4dfcf3ead3
usama <mahmoodify@gmail.com>
2022-09-09 14:12:37 +0500

    adding valid and invalid connection colors

--- src/ConnectivesLib/GlobalSettings/BlockSettings.cs
+++ src/ConnectivesLib/GlobalSettings/BlockSettings.cs
@@ -10,6 +10,9 @@ namespace Connectives.GlobalSettings
         // ------------
         public Color BlockBorderColor { get; set; } = Color.FromArgb(alpha: 200, red: 250, green: 35, blue: 35);
 
+        public Color ConnectionValidColor { get; set; } = Color.FromArgb(alpha: 200, red: 68, green: 188, blue: 10);
+        public Color ConnectionInvalidColor { get; set; } = Color.FromArgb(alpha: 200, red: 254, green: 50, blue: 101);
+
         // ---------------
         // Dimensions
         // ------------
