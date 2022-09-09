b6f929c985552df567904278f1d4d33db9baf6f5
usama <mahmoodify@gmail.com>
2022-09-09 14:06:59 +0500

    adding block border color

--- src/ConnectivesLib/GlobalSettings/BlockSettings.cs
+++ src/ConnectivesLib/GlobalSettings/BlockSettings.cs
@@ -1,6 +1,12 @@
-﻿namespace Connectives.GlobalSettings
+﻿using System.Drawing;
+
+namespace Connectives.GlobalSettings
 {
     public class BlockSettings
     {
+        // ---------------
+        // Colors
+        // ------------
+        public Color BlockBorderColor { get; set; } = Color.FromArgb(alpha: 200, red: 250, green: 35, blue: 35);
     }
 }
